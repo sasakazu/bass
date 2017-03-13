b2072e999e82a9f86bc4380110bb65eea5b6e63c
sasakazu <dropout.college2014@gmail.com>
2017-03-13 14:23:01 +0900

    bootstrap add done...

--- header.php
+++ header.php
@@ -6,13 +6,33 @@
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" media="screen">
 
-
-    <?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>
     <?php wp_head(); ?>
   </head>
 
   <body>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Bass, Inc.</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
 
-    <div class="container">
-      <div class="row">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">About</a></li>
+            <li><a href="#">Info</a></li>
+            <li><a href="#">Contact</a></li>
+
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
